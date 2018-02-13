355e909aff06a04bd48f6ded213e0c81a8f820ee
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-12 17:21:24 -0800

    Just checking to see if this works

--- main.cpp
+++ main.cpp
@@ -7,8 +7,17 @@ using namespace std;
 int main()
 {
   //declare variables
+  string first, last;
+
 
   //get user input
+  cout<< "What is your first name?\n";
+  cin>>first;
+
+  cout<< "What is your last name?\n";
+  cin>>last;
+
+cout<<"Hello " <<first<<" " <<last<<".";
 
   //tell fortune
 
