6d0742a37fe8aec0281427ff744077b2d43c2a92
keenanjacob <kjacob1@mail.csuchico.edu>
2018-02-14 19:13:07 -0800

    The final test

--- main.cpp
+++ main.cpp
@@ -29,25 +29,25 @@ int main()
 
   //tell fortune
 
-  cout<< "Your lucky number is " <<lucky<< ".\n";
+  cout<< "your lucky number is " <<lucky<< "\n";
 
   //fortune continued
 
 int len= last.length();
 
   if (first[0]=='a' || first[0]=='A' || first[0]=='e' || first[0]=='E' || first[0]=='i' || first[0]=='I' || first[0]=='o' || first[0]=='O' || first[0]=='u' || first[0]=='U')
-  {cout<< "You are destined to be famous!\n";
+  {cout<< "you are destined to be famous!\n";
   }
   else 
-  {cout<< "You should keep a low profile.\n";
+  {cout<< "you should keep a low profile.\n";
   }
 
   if (last[len-1]=='a' || last[len-1]=='e' || last[len-1]=='i' || last[len-1]=='o' || last[len-1]=='u')
   {
-  cout<< "You have already met your true love.\n";
+  cout<< "you have already met your true love.\n";
   }
 
-  cout<< "Have a good day!\n";
+  cout<< "have a good day!\n";
 
   return 0;
 
